d2998b37dabdad76fe9dfdb3ab30f04dd688911e
NereonNeo <nodirayakubova26@gmail.com>
2021-08-04 14:00:47 +0500

    feature: remove redux extension

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -6,12 +6,5 @@ const rootReducer = combineReducers({
 });
 
 //!Just store and add redux-devtools extencion
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 export default store;
